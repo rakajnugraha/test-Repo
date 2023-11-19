7248e71bb25aaa4bbbde93b2f39b5f180978d2fe
rakanugraha18 <rakanugraha2@gmail.com>
2023-11-19 18:53:11 +0700

    hapus css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { DynamicThemeProvider } from "./context/DynamicThemeContext";
-import { CartProvider } from "./context/CartProvider";
-import Header from "./components/organism/Header/Header";
-import Footer from "./components/organism/Footer";
 import { router } from "./routers";
 import { RouterProvider } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <DynamicThemeProvider>
-        <CartProvider>
-          <main>
-            <RouterProvider router={router} />
-            <Outlet />
-          </main>
-        </CartProvider>
-      </DynamicThemeProvider>
+      <main>
+        <RouterProvider router={router} />
+        <Outlet />
+      </main>
     </>
   );
 }
